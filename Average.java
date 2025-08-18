4a6028c783cc5ec2462ff30d9a099b02c1cd7e59
Meenakshi930 <meenakshigupta3009@gmail.com>
2025-08-18 16:50:17 +0530

    average

---
+++ Average.java
@@ -0,0 +1,9 @@
+public class Average {
+    public static void main(String[] args) {
+        float a = 5;
+        float b = 2;
+        float c = 1;
+        double average = (a+b+c)/3;
+        System.out.println(average);
+    }
+}
