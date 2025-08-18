c0f59696c5eaffdefd60874973e87657de44dafb
Meenakshi930 <meenakshigupta3009@gmail.com>
2025-08-18 16:48:00 +0530

    power calculation

---
+++ PowerCalculation.java
@@ -0,0 +1,8 @@
+public class PowerCalculation {
+    public static void main(String[] args) {
+      int base = 2 ;
+      int exponent = 3;
+      double power = Math.pow(base,exponent);
+      System.out.println(power);  
+    }
+}
