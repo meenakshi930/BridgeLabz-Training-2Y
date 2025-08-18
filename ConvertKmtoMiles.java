1d1aac8b79470f760a9fcc05bdc5237e84267bec
Meenakshi930 <meenakshigupta3009@gmail.com>
2025-08-18 16:37:34 +0530

    km to miles

---
+++ ConvertKmtoMiles.java
@@ -0,0 +1,7 @@
+public class ConvertKmtoMiles {
+    public static void main(String[] args) {
+        float km = 6;
+        double miles = km * 0.621371;
+        System.out.println(miles);
+    }
+}
---
+++ PerimeterofRectangle.java
@@ -0,0 +1,8 @@
+public class PerimeterofRectangle {
+    public static void main(String[] args) {
+        int length = 15;
+        int width = 10;
+        float perimeter = 2 * (length + width);
+        System.out.println(perimeter);
+    }
+}
