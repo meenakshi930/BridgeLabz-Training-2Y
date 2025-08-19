90704570a7551711fd07f2c2d73008c83e8491c9
meenakshi930 <meenakshigupta3009@gmail.com>
2025-08-19 11:27:42 +0530

    age

---
+++ km to miles.java
@@ -0,0 +1,8 @@
+ class kmtomiles {
+    public static void main(String[] args) {
+        double km=10.8;
+        double miles= 0.621371*km;
+        System.out.println("The distance 10.8 km in miles is "+miles);
+    }
+    
+}
