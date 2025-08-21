467377d2e5dcd0a28cb3619f8b3d36931c131135
meenakshi930 <meenakshigupta3009@gmail.com>
2025-08-21 12:16:28 +0530

    refactoring folder

--- level 1/age.java
+++
@@ -1,7 +0,0 @@
-class age {
-    public static void main(String[] args) {
-        int bornyear =2000;
-        int currentage = 2024-bornyear;
-        System.out.println("Harry's age in 2024 is "+currentage); 
-}
-}
