7111ee93ef876e22a48e9f401de4c11af97e4a94
meenakshi930 <meenakshigupta3009@gmail.com>
2025-08-21 10:42:53 +0530

    questions

--- level 1/calculator.java
+++ level 1/calculator.java
@@ -3,11 +3,11 @@ class calculator {
     public static void main(String[] args) {
          Scanner input = new Scanner(System.in);
         double number1 ;
-        System.out.print("number: ");
+        System.out.print("number1: ");
         number1= input.nextInt();
          Scanner input1 = new Scanner(System.in);
         double number2;
-        System.out.print("number: ");
+        System.out.print("number2: ");
         number2= input1.nextInt();
         double add = number1 + number2;
         double subtract = number1-number2;
